11fcb3838b4c119069fc02d15326ef6cb8edd6e8
akmalkhalil <akmal.khalil.97@gmail.com>
2020-09-04 21:18:50 +0100

    button generates 30led strip geometry, I hid canvas and walls in unity, not sure what I ddi to .mat file

--- LED Visualiser/Assets/Scripts/NewLEDButton.cs
+++ LED Visualiser/Assets/Scripts/NewLEDButton.cs
@@ -12,7 +12,37 @@ public class NewLEDButton : MonoBehaviour
 
 
     public void newLEDStrip () {
-        // TODO: generate LED strip, not just a single LED
+        // int stripLen = 1; // 1m
+        int numLEDs = 30;
+        float stripLen = 1.5f; //temp until we set world dimensions
+
+        GameObject strip = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        strip.transform.localScale = new Vector3(stripLen,0.008f,0.025f);
+
+
+        /***  generate individual cells on the strip  ***/
+        
+        GameObject[] leds = new GameObject[numLEDs]; 
+        // TODO: move all this to an initLED function?
+        float w = 0.005f;
+        Vector3 ledDim = new Vector3(w, 0.001f, w);
+        // stripLen = 30*w + 30*gap, edge = gap/2
+        float gap = 0.045f; // TODO: this should be dependant on numLEDs
+        float edge = 0.00225f;    
+        // This for loop could very nicely be parrallelised
+        for (int i = 0; i < numLEDs; i++){
+            leds[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            // TODO: parent all these gameobjects to strip and give each unique identifier (like LED-i)
+            leds[i].transform.localScale = ledDim; //  does this assign by val or assign ref? maybe do localScale *= ledDim?
+            float x = edge +  i * (gap+w)  -stripLen/2 ;
+            leds[i].transform.position = new Vector3(x, 0.001f, 0.0f);
+        }
+
+        // TODO: parent the LEDs to the strip
+
+        /***  address cells on the strip  ***/
+        // TODO: basically copy the bit below abuot the text field to get the first address, then every pixels address after will be prev+3
+
     }
 
     public void newLED(){
@@ -34,17 +64,6 @@ public class NewLEDButton : MonoBehaviour
 
 
 
-
-
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
 }
+
+
